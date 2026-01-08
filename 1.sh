af2f7dc49df514dbbc67b50fc6e5b2eb29ee0fe3
Pablo <pagira@alumnatflorida.es>
2026-01-08 09:32:25 +0100

    añadiendo nombre

---
+++ 1.sh
@@ -0,0 +1,7 @@
+read -p "Escribe tu nombre: " nombre
+read -p 'Escribe tu primer apellido: ' apellido1
+read -p 'Escribe tu segundo apellido: ' apellido2
+
+sleep 1
+
+echo "Tu nombre completo es $nombre $apellido1 $apellido2"
